515d06a947cd933dfc24b4a71ed92069eac2ed97
things23 <twothreethings@gmail.com>
2015-01-29 16:37:17 +0300

    List books with raying filter

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -1,16 +1,26 @@
 require 'test_helper'
 
 class ListingBooksTest < ActionDispatch::IntegrationTest
+  setup do
+    Book.create!(title: 'Pragmatic Programmer', rating: 5)
+    Book.create!(title: "Ender's game", rating: 3)
+  end
+
   test 'listing books' do
-    setup do
-      Book.create!(title: 'Pragmatic Programmer', rating: 5)
-      Book.create!(title: "Ender's game", rating: 3)
-    end
     get '/books'
 
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
 
     assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'lists to rated books' do
+    get '/books?rating=5'
+
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    assert_equal 1, JSON.parse(response.body).size
   end
 end
